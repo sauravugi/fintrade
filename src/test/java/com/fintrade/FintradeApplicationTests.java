bf9f063b1cf67af2b7c4d448488b7431f9888928
Saurav Shah <saurav@Mac.lan>
2025-09-30 09:35:51 +0530

    Initial commit - FinTrade project

---
+++ src/test/java/com/fintrade/FintradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fintrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
